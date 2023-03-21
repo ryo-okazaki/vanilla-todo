abea31c90bc36dd76334ba960ac230c98fcdd60c
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-21 13:01:54 +0900

    完了ボタンの機能を追加

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,30 @@ const onClickAdd = () => {
   completeButton.addEventListener("click", () => {
     // 押下された完了ボタンの親タグ(div)を未完了リストから削除
     deleteFromIncompleteList(completeButton.parentNode);
+
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // liタグを生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button(削除)タグを生成
