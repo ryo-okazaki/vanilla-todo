f3bf2a7e2791dccc23ba0e96a3e8074c043400a6
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-21 11:23:03 +0900

    完了ボタン、削除ボタン動作確認

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,11 +13,17 @@ const onClickAdd = () => {
 
   // button(完了)タグを生成
   const completeButton = document.createElement("button");
-  completeButton.innerText = "完了"
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
 
   // button(削除)タグを生成
   const deleteButton = document.createElement("button");
-  deleteButton.innerText = "削除"
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
