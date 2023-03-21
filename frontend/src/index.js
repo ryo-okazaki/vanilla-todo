1ce89fb8b66ba6c55bcc84e6540b2d359631916f
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-21 13:22:16 +0900

    未完了TODO処理の関数を共通化

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,23 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+}
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+}
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // div生成
   const div = document.createElement("div");
   div.className = "list-row";
 
   // liタグ生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // button(完了)タグを生成
   const completeButton = document.createElement("button");
@@ -66,11 +76,6 @@ const onClickAdd = () => {
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
-}
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 }
 
 document
