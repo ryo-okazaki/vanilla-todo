3fc87510562457c9f243ad07223d7e9394ff847c
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-21 13:02:59 +0900

    戻すボタンの動作確認

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -34,6 +34,9 @@ const onClickAdd = () => {
     // buttonタグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      alert("戻す");
+    })
 
     // divタグの子要素に各要素を設定
     addTarget.appendChild(li);
